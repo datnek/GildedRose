f1fe4c8263c85bcc6d36e2dc7b01410fb531e24c
Danick Takam <213346@supinfo.com>
2020-02-18 00:49:43 +0100

    ItemService added

--- csharpcore/Services/BaseItemService.cs
+++ csharpcore/Services/BaseItemService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using csharpcore.Utils;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
---
+++ csharpcore/Services/ItemService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharpcore.Services
+{
+    /// <summary>
+    /// Manage All basics services
+    /// </summary>
+    public class ItemService : BaseItemService, IItemService
+    {
+        public void DecreaseQuality()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
