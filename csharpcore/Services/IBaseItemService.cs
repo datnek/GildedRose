2d217ff881805791434660aabdd367c648ac3a8b
Danick Takam <213346@supinfo.com>
2020-02-17 23:58:48 +0100

    IBaseItemService added

---
+++ csharpcore/Services/IBaseItemService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharpcore.Services
+{
+    public interface IBaseItemService
+    {
+        void UpdateItem(bool updateSellIn = true);
+        void DecreaseSellin();
+        Item Item { get; set; }
+    }
+}
