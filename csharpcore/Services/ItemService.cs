689ae92ea13f446bdfad7a214fda3a13d707dcac
Danick Takam <213346@supinfo.com>
2020-02-18 00:51:47 +0100

    DecreaseQuality added

--- csharpcore/Services/ItemService.cs
+++ csharpcore/Services/ItemService.cs
@@ -9,9 +9,17 @@ namespace csharpcore.Services
     /// </summary>
     public class ItemService : BaseItemService, IItemService
     {
-        public void DecreaseQuality()
-        {
-            throw new NotImplementedException();
-        }
+        #region "DecreaseQuality"
+        /// <summary>
+        /// The DecreaseQuality method solves the following scenarios
+        /// 1- The Quality of an item is never negative
+        /// 2- At the end of each day, our system lowers the quality for each item
+        /// 3- "Sulfuras", being a legendary item, never has to be  decreases in Quality
+        /// </summary>
+        public virtual void DecreaseQuality()
+            => Item.Quality = Item.Name != Constants.SULFURAGES_ITEM
+                                && Item.Quality > 0 ?
+                                Item.Quality - 1 : Item.Quality;
+        #endregion
     }
 }
