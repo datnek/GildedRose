cc29ed1149a6778de3fea7a4468d4b9614145ae0
Danick Takam <213346@supinfo.com>
2020-02-18 00:08:33 +0100

    DecreaseQualityOneDayForDefaultItem added

--- csharpcore.Tests/ItemServiceUnitTest.cs
+++ csharpcore.Tests/ItemServiceUnitTest.cs
@@ -4,8 +4,33 @@ using System.Text;
 
 namespace csharpcore.Tests
 {
-   public class ItemServiceUnitTest
+    public class ItemServiceUnitTest
     {
+        public virtual ItemService ItemService { get; set; }
+        public ItemServiceUnitTest()
+        {
 
+            //Arrange 
+            ItemService = new ItemService()
+            {
+                Item = new Item
+                {
+                    Name = "Foo",
+                    SellIn = 10,
+                    Quality = 20
+                }
+            };
+        }
+
+
+        [TestMethod]
+        public virtual void DecreaseQualityOneDayForDefaultItem()
+        {
+            // Act
+            ItemService.DecreaseQuality();
+
+            // Assert 
+            Assert.AreEqual(ItemService.Item.Quality, 19);
+        }
     }
 }
