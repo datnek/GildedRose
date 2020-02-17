0195ec22421972075837acafa86488b117d5f0d8
Danick Takam <213346@supinfo.com>
2020-02-18 00:14:48 +0100

    DecreaseSellinOneDay, DecreaseQualityOneDayWithQuality0 added

--- csharpcore.Tests/ItemServiceUnitTest.cs
+++ csharpcore.Tests/ItemServiceUnitTest.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace csharpcore.Tests
 {
+    [TestClass]
     public class ItemServiceUnitTest
     {
         public virtual ItemService ItemService { get; set; }
@@ -32,5 +34,30 @@ namespace csharpcore.Tests
             // Assert 
             Assert.AreEqual(ItemService.Item.Quality, 19);
         }
+
+        [TestMethod]
+        public virtual void DecreaseQualityOneDayWithQuality0()
+        {
+            // Arrange
+            ItemService.Item.Quality = 0;
+
+            // Act
+            ItemService.DecreaseQuality();
+
+            // Assert 
+            Assert.AreEqual(ItemService.Item.Quality, 0);
+        }
+
+
+        [TestMethod]
+        public virtual void DecreaseSellinOneDay()
+        {
+            // Act
+            ItemService.DecreaseSellin();
+
+            // Assert 
+            Assert.AreEqual(ItemService.Item.SellIn, 9);
+        }
+
     }
 }
