0e11287ac168b0ccd142f841459afc59f98635dc
Danick Takam <213346@supinfo.com>
2020-02-18 00:36:57 +0100

    DecreaseSellin added

--- csharpcore/Services/BaseItemService.cs
+++ csharpcore/Services/BaseItemService.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace csharpcore.Services
 {
-    class BaseItemService
+    public  class BaseItemService : IBaseItemService
     {
+        public Item Item { get; set; }
+
+        #region "DecreaseSellin"
+        /// <summary>
+        /// /// The DecreaseSellin method solves the following scenarios
+        /// 1- At the end of each day, our system lowers the SellIn for each item
+        /// 2- "Sulfuras", being a legendary item, never has to be sold 
+        /// </summary>
+        /// </summary>
+        public virtual void DecreaseSellin()
+            => Item.SellIn = Item.Name != Constants.SULFURAGES_ITEM ?
+                            Item.SellIn - 1 : Item.SellIn;
+        #endregion
+
+        public void UpdateItem(bool updateSellIn = true)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
