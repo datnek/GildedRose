098111b05c159595a7c699b96c5a4fb87b53d424
Danick Takam <213346@supinfo.com>
2020-02-18 00:05:04 +0100

    ItemServiceUnitTest and constructor added

---
+++ csharpcore.Tests/ItemServiceUnitTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharpcore.Tests
+{
+   public class ItemServiceUnitTest
+    {
+
+    }
+}
--- csharpcore/Services/IItemService.cs
+++ csharpcore/Services/IItemService.cs
@@ -4,7 +4,8 @@ using System.Text;
 
 namespace csharpcore.Services
 {
-    class IItemService
+    public interface IItemService : IBaseItemService
     {
+        void DecreaseQuality();
     }
 }
