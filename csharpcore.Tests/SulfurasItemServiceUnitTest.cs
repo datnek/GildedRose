284a405c0dd36e0d9521605f4b4cd23e634aae99
Danick Takam <213346@supinfo.com>
2020-02-18 00:24:55 +0100

    SulfurasItemServiceUnitTest added

---
+++ csharpcore.Tests/SulfurasItemServiceUnitTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharpcore.Tests
+{
+    [TestClass]
+    public class SulfurasItemServiceUnitTest
+    {
+    }
+}
