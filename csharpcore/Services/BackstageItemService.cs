10ca199175521a464afc70ef003e77f143cfa0fa
Danick Takam <213346@supinfo.com>
2020-02-18 01:41:15 +0100

    BackstageItemService added

---
+++ csharpcore/Services/BackstageItemService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharpcore.Services
+{
+    public class BackstageItemService : AgedItemService, IBackstageItemService
+    {
+
+    }
+}
