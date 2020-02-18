e28eb5eadc428d30ec10f2c7b0cbdc566baaf2df
Danick Takam <213346@supinfo.com>
2020-02-18 01:16:56 +0100

    IAgedItemService added

---
+++ csharpcore/Services/IAgedItemService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharpcore.Services
+{
+    public interface IAgedItemService : IBaseItemService
+    {
+        void IncreaseQuality();
+    }
+}
