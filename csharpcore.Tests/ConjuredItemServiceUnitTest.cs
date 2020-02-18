a9576f120d8c030caf4343b1a8a8f4030e00a98e
Danick Takam <213346@supinfo.com>
2020-02-18 01:51:49 +0100

    ConjuredItemServiceUnitTest added

---
+++ csharpcore.Tests/ConjuredItemServiceUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using csharpcore.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharpcore.Tests
+{
+    [TestClass]
+    public class ConjuredItemServiceUnitTest : ItemServiceUnitTest
+    {
+        public ConjuredItemServiceUnitTest() : base()
+        {
+            //Arrange 
+            ItemService = new ConjuredItemService()
+            {
+                Item = new Item
+                {
+                    Name = Constants.CONJURED_ITEM,
+                    SellIn = 10,
+                    Quality = 20
+                }
+            };
+        }
+
+    }
+}
