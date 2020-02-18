5b14a0bc9155c0f2974c3bf1e4a7a8d48523d99c
Danick Takam <213346@supinfo.com>
2020-02-18 02:43:33 +0100

    showitem updated

--- csharpcore/Program.cs
+++ csharpcore/Program.cs
@@ -58,18 +58,16 @@ namespace csharpcore
 
             Console.WriteLine("OMGHAI!");
 
-
             for (var i = 0; i < 31; i++)
             {
                 Console.WriteLine("-------- day " + i + " --------");
                 Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < Items.Count; j++)
-                {
-                    System.Console.WriteLine(Items[j].Name + ", " + Items[j].SellIn + ", " + Items[j].Quality);
-                }
+                app.ShowItems();
                 Console.WriteLine("");
                 app.UpdateQuality();
             }
+
+            serviceProvider.Dispose();
         }
     }
 }
