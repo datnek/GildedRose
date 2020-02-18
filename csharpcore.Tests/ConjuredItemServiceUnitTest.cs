63004cd7c55cea933e282666c4f4c93bd92cd5f2
Danick Takam <213346@supinfo.com>
2020-02-18 01:52:55 +0100

    UpdateItemOneDayWithNegatifSellIn added

--- csharpcore.Tests/ConjuredItemServiceUnitTest.cs
+++ csharpcore.Tests/ConjuredItemServiceUnitTest.cs
@@ -23,5 +23,22 @@ namespace csharpcore.Tests
             };
         }
 
+        [TestMethod]
+        public override void UpdateItemOneDayWithNegatifSellIn()
+        {
+            // Arrange
+            ItemService.Item.SellIn = -1;
+
+
+            // Act
+            ItemService.UpdateItem();
+
+            // Assert 
+            Assert.AreEqual(ItemService.Item.Quality, 16);
+
+            // Assert 
+            Assert.AreEqual(ItemService.Item.SellIn, -2);
+        }
+
     }
 }
