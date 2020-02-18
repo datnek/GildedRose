1f6d9bb39f7f0f3096887518166a6dc25bb75366
Danick Takam <213346@supinfo.com>
2020-02-18 01:34:09 +0100

    UpdateItemOneDayWithPositifSellIn added

--- csharpcore.Tests/BackstageItemServiceUnitTest.cs
+++ csharpcore.Tests/BackstageItemServiceUnitTest.cs
@@ -1,10 +1,44 @@
-﻿using System;
+﻿using csharpcore.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace csharpcore.Tests
 {
-    class BackstageItemServiceUnitTest
+    [TestClass]
+    public class BackstageItemServiceUnitTest : AgedItemServiceUnitTest
     {
+
+        public BackstageItemServiceUnitTest() : base()
+        {
+            // Arrange
+            ItemService = new BackstageItemService()
+            {
+                Item = new Item
+                {
+                    Name = Constants.BACKSTAGE_ITEM,
+                    SellIn = 10,
+                    Quality = 20
+                }
+            };
+        }
+
+        [TestMethod]
+        public override void UpdateItemOneDayWithPositifSellIn()
+        {
+            //Arrange 
+            ItemService.Item.SellIn = 16;
+
+            // Act
+            ItemService.UpdateItem();
+
+            // Assert 
+            Assert.AreEqual(ItemService.Item.Quality, 21);
+
+            // Assert 
+            Assert.AreEqual(ItemService.Item.SellIn, 15);
+        }
+
     }
 }
