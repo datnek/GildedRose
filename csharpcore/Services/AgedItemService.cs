0a20b93a07a0812fb4634d7396b4e611d6462c59
Danick Takam <213346@supinfo.com>
2020-02-18 01:22:09 +0100

    IncreaseQuality added

--- csharpcore/Services/AgedItemService.cs
+++ csharpcore/Services/AgedItemService.cs
@@ -6,6 +6,14 @@ namespace csharpcore.Services
 {
     public class AgedItemService : BaseItemService, IAgedItemService
     {
-
+        #region "IncreaseQuality"
+        /// <summary>
+        /// 1- The Quality of an item is never more than 50
+        /// 2- "Aged Brie" actually increases in Quality the older it gets
+        /// </summary>
+        public virtual void IncreaseQuality()
+            => Item.Quality = Item.Quality < 50 ?
+                                 ++Item.Quality : Item.Quality;
+        #endregion
     }
 }
