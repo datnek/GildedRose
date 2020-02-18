eca8ecf1e982f6eb8bd29202fa32332cc296f0b9
Danick Takam <213346@supinfo.com>
2020-02-18 02:03:31 +0100

    IGildedRoseService added

---
+++ csharpcore/Services/IGildedRoseService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharpcore.Services
+{
+    public interface IGildedRoseService
+    {
+        void UpdateQuality();
+        void SetItem(IList<Item> items);
+        void ShowItems();
+    }
+}
