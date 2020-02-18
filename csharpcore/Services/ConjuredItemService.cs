32e196d6eda76ff615b0111c619cae91f4e6902e
Danick Takam <213346@supinfo.com>
2020-02-18 01:59:21 +0100

    UpdateItem added

--- csharpcore/Services/ConjuredItemService.cs
+++ csharpcore/Services/ConjuredItemService.cs
@@ -4,7 +4,8 @@ using System.Text;
 
 namespace csharpcore.Services
 {
-    class ConjuredItemService : ItemService, IConjuredItemService
+    public class ConjuredItemService : ItemService, IConjuredItemService
     {
+
     }
 }
