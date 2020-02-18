3434ecb508c81f69924b9087f4dc529df4c86bda
Danick Takam <213346@supinfo.com>
2020-02-18 01:04:32 +0100

    IncreaseQualityOneDayForQuality50Item added

--- csharpcore.Tests/AgedItemServiceUnitTest.cs
+++ csharpcore.Tests/AgedItemServiceUnitTest.cs
@@ -1,10 +1,41 @@
-﻿using System;
+﻿using csharpcore.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace csharpcore.Tests
 {
-    class AgedItemServiceUnitTest
+    [TestClass]
+    public class AgedItemServiceUnitTest
     {
+        public virtual IAgedItemService ItemService { get; set; }
+        public AgedItemServiceUnitTest()
+        {
+            // Arrange
+            this.ItemService = new AgedItemService()
+            {
+                Item = new Item
+                {
+                    Name = Constants.AGED_ITEM,
+                    SellIn = 10,
+                    Quality = 20
+                }
+            };
+        }
+
+        [TestMethod]
+        public virtual void IncreaseQualityOneDayForQuality50Item()
+        {
+            // Arrange
+            ItemService.Item.Quality = 50;
+
+            // Act
+            ItemService.IncreaseQuality();
+
+            // Assert 
+            Assert.AreEqual(ItemService.Item.Quality, 50);
+        }
+
     }
 }
