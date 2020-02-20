8f6fa8e119ac687b29647ee2fe90f77551138101
Danick Takam <213346@supinfo.com>
2020-02-20 21:08:22 +0100

    test updated

--- csharpcore.Tests/Services/BackstageItemServiceUnitTest.cs
+++ csharpcore.Tests/Services/BackstageItemServiceUnitTest.cs
@@ -6,7 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace csharpcore.Tests
+namespace csharpcore.Tests.Services
 {
     [TestClass]
     public class BackstageItemServiceUnitTest : AgedItemServiceUnitTest
--- csharpcore.Tests/Services/ItemServiceUnitTest.cs
+++ csharpcore.Tests/Services/ItemServiceUnitTest.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace csharpcore.Tests
+namespace csharpcore.Tests.Services
 {
     [TestClass]
     public class ItemServiceUnitTest
